29c03533e326b49fd9330ab865d1bee35276a6fc
K_PU <xl1816134052@163.com>
2026-07-11 20:00:45 +0800

    chore(config): add .gitattributes to set LF line endings for build.sh / 添加 .gitattributes 配置以统一 build.sh 换行符

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-./.venv-linux/bin/python -m nuitka \
+./.venv/Scripts/python.exe -m nuitka \
     --onefile \
     --enable-plugin=tk-inter \
     --include-package=textual \
     --include-module=sounddevice \
-    --include-data-dir=./.venv-linux/lib/python3.12/site-packages/_sounddevice_data=_sounddevice_data \
+    --include-data-dir=./.venv/Lib/site-packages/_sounddevice_data=_sounddevice_data \
     --include-package=imageio_ffmpeg \
     --include-package-data=imageio_ffmpeg \
     --output-filename=PyAsciiFilm \
-    --output-dir=build/Linux_64 \
+    --output-dir=build/Win_64 \
     --python-flag=-O \
     --lto=yes \
     ./src/main.py
