327b81e6e2ca93455804fd88239a7883576d0858
K_PU <xl18161234052@163.com>
2026-07-14 20:17:41 +0800

    -

--- build-linux_x64.sh
+++ build-linux_x64.sh
@@ -11,7 +11,7 @@ VERSION="${VERSION:-0.0.0}"
     --include-package=rich._unicode_data \
     --include-package=imageio_ffmpeg \
     --include-module=miniaudio \
-    --output-filename="PyAsciiFilm-${VERSION}-linux_x64" \
+    --output-filename="PyAsciiFilm-v${VERSION}-linux_x64" \
     --output-dir="build/linux_x64" \
     --lto=yes \
     ./src/main.py
